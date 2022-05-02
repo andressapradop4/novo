e85b8e10034e5bb1c05c8f061dd76504b104a534
andressa <andressa@LAPTOP-FN9LIPF8>
2022-05-02 19:34:37 -0300

    racha a cuca de novo...

---
+++ exercicio2/src/degustacaovinho.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/*A primeira linha contém um inteiro T representando o tipo de
+        vinho (1 ≤ T ≤ 4). A segunda linha contém cinco inteiros A, B, C,
+D e E, que indica a resposta dada por cada 
+        competidor (1 ≤ A, B, C, D, E ≤ 4).
+*/
+
+public class degustacaovinho {
+
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    String T = scan.nextLine();
+    String respostasCompetidores = scan.nextLine();
+
+    String[] respostasCompetidoresSplit = respostasCompetidores.split(" ");
+//TODO: Complete o código com uma possível solução para o problema.    
+    int count = 0;
+    for (                         ) {
+      if (                         ) 
+        count++;
+    }
+    
+    System.out.println(           );
+  }
+    }
+    
+
