b733b2a9c3bf5fd9fae31ac9e3ac95433fab9e1d
andressa <andressa@LAPTOP-FN9LIPF8>
2022-05-02 19:48:04 -0300

    veja o fatorial de 5

---
+++ fatorial/src/fatorial/Fatorial.java
@@ -0,0 +1,14 @@
+
+package fatorial;
+
+
+public class Fatorial {
+
+    
+    public static void main(String[] args) {
+        System.out.println("fatorial");
+             
+        
+    }
+    
+}
