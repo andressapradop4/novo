550d5ab81080cab9a7d17f7ea9327aac665928b9
andressa <andressa@LAPTOP-FN9LIPF8>
2022-05-02 19:01:58 -0300

    escrever o nome

---
+++ exercicios1/src/exercicios1/Exercicios1.java
@@ -0,0 +1,13 @@
+
+package exercicios1;
+
+import java.util.Scanner;
+public class Exercicios1 {
+
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       String nome;
+        System.out.println("escreva o seu nome");
+       nome = in.nextLine();
+    }   
+}
